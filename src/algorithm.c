4f325ad3e4fd87577c192c3adb17f227145f877c
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-30 00:02:25 -0300

    skipping pb of the last stack-a element to reduce the number of op by 2

--- src/algorithm.c
+++ src/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:00:39 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/03/29 22:00:28 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/03/29 23:51:42 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,6 @@ void	bubbling_up(t_stack *a, t_stack *b, t_tool *tool)
 		}
 		index++;
 	}
-	do_two_stacks_operation(a, b, pb, tool);
 }
 
 int	is_sorted(t_stack *a)
@@ -54,7 +53,7 @@ int	is_sorted(t_stack *a)
 	int	i;
 
 	i = 0;
-	while (i < a->last_index)
+	while (i <= a->last_index)
 	{
 		if (a->stack[i] > a->stack[i + 1])
 			return (0);
@@ -65,12 +64,15 @@ int	is_sorted(t_stack *a)
 
 void	algorithm(t_stack *a, t_stack *b, t_tool *tool)
 {
-	if (!is_sorted(a))
+	if (!is_sorted(a) && a->last_index > 0)
 	{
 		bubbling_up(a, b, tool);
+		do_two_stacks_operation(a, b, pb, tool);
 		algorithm(a, b, tool);
 	}
 	else
+	{
 		while (b->last_index >= 0)
 			do_two_stacks_operation(a, b, pa, tool);
+	}
 }
