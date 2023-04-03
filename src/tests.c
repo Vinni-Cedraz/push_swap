650685cfa7e8fd38c1ce0f3e14acec4ba34eacbb
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-04-03 08:55:50 -0300

    Working only for reverse ordered lists
    And bash had nothing to do with it.
    Still, even in the case it works (reversed lists) it is always doing
    rrb even when it should be doing rb.

--- src/tests.c
+++ src/tests.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 14:53:06 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/04/03 00:32:56 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/04/03 08:47:34 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	debug_footer(t_stack *a, t_tool *tool)
 	ft_putstr("---------------\n");
 	ft_putstr_fd("total number of operations: ", 1);
 	ft_putnbr_fd(tool->count, 1);
+	ft_putchar('\n');
 }
 
 void do_one_stack_operation(t_stack *a, t_stack *b, t_funct1 f, t_tool *tool)
@@ -39,9 +40,9 @@ void do_one_stack_operation(t_stack *a, t_stack *b, t_funct1 f, t_tool *tool)
 	if (tool->which == B)
 		(f)(b, 0);
 	tool->count++;
-	ft_putstr("--------\n");
+	ft_putstr("---------------\n");
 	print_stacks(a, b);
-	ft_putstr("--------\n");
+	ft_putstr("---------------\n");
 	return ;
 }
 
@@ -51,8 +52,8 @@ void	do_two_stacks_operation(t_stack *a, t_stack *b, t_funct2 f,
 	(f)(a, b);
 	if (is_sorted(a))
 		tool->last_pa = tool->count;
-	ft_putstr("--------\n");
+	ft_putstr("----------------\n");
 	print_stacks(a, b);
-	ft_putstr("--------\n");
+	ft_putstr("----------------\n");
 	tool->count++;
 }
