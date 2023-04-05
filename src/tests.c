f556ac673604a8545fba89b59a90dcbe1c1a02b5
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-04-05 16:12:22 -0300

    Passing all tests with up to a 100 elements
    Score is low on 100 elements and we still don't pass with 500
    -> will probably score high when we merge the rotations with rr and rrr
    -> or at least pass on 500
    -> If not, then we have to look for mistakes in the algorithm
    implementation first before thinking of changing it.

--- src/tests.c
+++ src/tests.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 14:53:06 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/04/05 12:55:55 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/04/05 16:08:05 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,15 @@ void	do_first_pushes(t_stack *a, t_stack *b, t_tool *tool)
 {
 	int	i;
 
-	i = -1;
-	while (++i < 2)
+	i = 0;
+	while (i++ < 3)
 	{
 		if (is_lowest_three(tool->lowest_three, a->stack[a->last_index]))
 			do_one_stack_operation(a, b, ra, tool);
-		else
-			do_two_stacks_operation(a, b, pb, tool);
+		if (a->stack[a->last_index] == get_biggest(a))
+			do_one_stack_operation(a, b, ra, tool);
+		if (is_lowest_three(tool->lowest_three, a->stack[a->last_index]))
+			do_one_stack_operation(a, b, ra, tool);
 	}
 	if (is_lowest_three(tool->lowest_three, a->stack[a->last_index]))
 		do_one_stack_operation(a, b, ra, tool);
